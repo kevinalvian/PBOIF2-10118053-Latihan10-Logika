dcc29a3444655186102e9f7f56ce24e45a56b5aa
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-09-26 08:22:12 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118053/latihan10/logika/PBOIF210118053Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118053.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA : Kevin Alvian
+ * KELAS : IF2
+ * NIM : 10118053
+ */
+public class PBOIF210118053Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+
+    }
+    
+}
